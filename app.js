991775c35d50b3f83e6a6d96da9de600ea5993c2
Ben Elwood <ben@bens-m2-pro.local>
2023-11-01 13:26:46 +1300

    added confetti file and import

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ Last Modified: 14th Sept 2023 6:12 PM
 
 import './styles.css';
 import './partials/sidebar.js';
-import './partials/bedroom.js';
+import './partials/confetti.js';
+// import './partials/bedroom.js';
 // import './partials/boids.js';
-console.log('connected');
---
+++ partials/confetti.js
@@ -0,0 +1,1 @@
+console.log('confetti.js loaded');
